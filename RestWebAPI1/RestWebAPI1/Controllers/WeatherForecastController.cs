fda75cc68ff6cfd8885cc8fa1d62d494311a7e42
Devendra Choudhary <dk_chy1981@yahoo.co.in>
2024-12-31 14:46:14 +0530

    Added

--- RestWebAPI1/RestWebAPI1/Controllers/WeatherForecastController.cs
+++ RestWebAPI1/RestWebAPI1/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace RestWebAPI1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "DKC", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
